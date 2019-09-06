b77073e78531a6ace1a3171a5b1dacb965f1b7dc
BlackAce <alfredo.gon195@gmail.com>
2019-09-06 01:56:13 -0700

    added questions, choices and answers into a var in a funnction

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    //to hold the different questions and answers 
+    var options = [
+        {
+            question: "What is the fastest production motorcycle ever made?",
+            choice: ["BMW S1000RR HP4", "Ducati Panigale 1199R", "Yamaha YZF-R1M", "Kawasaki Ninja H2R"],
+            answer: 3,
+            // add photo
+        },
+        {
+            question: "What is the common gear count in a motorcycle?",
+            choice: ["4", "7", "6", "10"],
+            answer: 2,
+            // add photo
+        },
+        {
+            question: "how many cc's are needed to be considered a litter bike?",
+            choice: ["1000cc", "600cc", "300cc", "750cc"],
+            answer: 0,
+            // add photo
+        },
+        {
+            question: "what is a stoppie?",
+            choice: ["popping the front tire of the ground", "lifting the rear tire in the air", "slam on breaks", "normal stop"],
+            answer: 1,
+            // add photo
+        }]; 
+        // add var to hold values for functions and if statements
+})
+
+
+
